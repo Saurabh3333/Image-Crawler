c456a7de9af016c36c0c8361f9ef98c88dded216
Saurabh Shubham <saurabh.friday@gmail.com>
2020-08-22 23:59:18 +0530

    UI

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var request = require('request');
 var router = express.Router();
 var bodyParser = require('body-parser');
 var path = require("path");
-
+var fs = require("fs");
+const del = require('del');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -12,10 +13,27 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'crawl-result')));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+
+app.get('/crawler', (req, res) => {
+    res.sendFile(__dirname + '/crawl-result.html');
+});
+
+app.post('/crawler', (req, res) => {
+    console.log('URL: '+req.body.crawl_url);
+    res.redirect("/crawler");
+  });
+
+  app.post('/delete', (req, res) => {
+    console.log("Deleting previous results.....");
+    del.sync(['crawl-result/**', '!public/assets', '!public/assets/goat.png']);
+    console.log("Previous results deleted");
+    res.redirect("/");
+  });
 
 app.listen(8080, function () {
   console.log('App listening on port 8080!');
